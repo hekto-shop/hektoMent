cda92b45153c9e2e0ce14c2a954f2b46c2e1a20c
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-01-21 20:58:50 +0400

    feat: install React Router package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App () {
-    return (
-      <div>
-        Test Change
-      </div>
-    );
+function App() {
+  return (
+    <>
+      <div>Test Change</div>
+      <Route path="/header">
+        <p>Header</p>
+      </Route>
+    </>
+  );
 }
 
-export default App
+export default App;
