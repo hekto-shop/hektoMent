0f56594ed67776282f8aca536f4628336aea8761
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-01-27 12:45:39 +0400

    fix: horizontal scroll problem

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,26 @@
-import { ClassNames } from "@emotion/react";
 import React from "react";
+import * as icons from "../../assets/icons";
+
 import classes from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <section className={classes["bottom-navigation"]}></section>
-      <section className={classes.copyright}></section>
+      <section className={classes.copyright}>
+        <div className={classes.author}>©Webecy - All Rights Reserved</div>
+        <div className={classes.social}>
+          <a href="http://facebook.com" target="_blank">
+            <img src={icons.facebook} alt="Facebook" />
+          </a>
+          <a href="http://instagram.com" target="_blank">
+            <img src={icons.instagram} alt="Instagram" />
+          </a>
+          <a href="http://twitter.com" target="_blank">
+            <img src={icons.twitter} alt="Twitter" />
+          </a>
+        </div>
+      </section>
     </footer>
   );
 };
