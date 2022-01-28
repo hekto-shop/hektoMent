316db230f3178b85a4ae946f2fa5d2d7bb1319fa
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-01-28 20:52:25 +0400

    feat: update routing and add common layout to pages

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageLayout from "../../containers/PageLayout";
+
+const Contact = () => {
+  return (
+    <PageLayout title="Contact">
+      <h2>Contact Page</h2>
+    </PageLayout>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PageLayout from "../../containers/PageLayout";
+const Products = () => {
+  return (
+    <PageLayout title="Products">
+      <h1>Products List</h1>
+    </PageLayout>
+  );
+};
+
+export default Products;
