1ac1a5768a8e3c3be234015a79391bdb35489e23
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-02-02 19:25:25 +0400

    feat: Add sort functionality to Shop page

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -5,60 +5,71 @@ import * as icons from "../../assets/icons";
 
 import classes from "./Shop.module.scss";
 
-import GridView from "../../containers/GridView";
-import ListView from "../../containers/ListView/ListView";
+import GridView from "../../components/GridView";
+import ListView from "../../components/ListView";
 
 const Shop = () => {
   const [gridView, setGridView] = useState(true);
-  const productList = useSelector((state) => state.productsReducer.products);
+  const [sort, setSort] = useState(0); // 0-Arrival date; 1-Price:Lowest->Highest; 2-Price:Highest->Lowest;
+  const products = useSelector((state) => state.productsReducer.products);
 
-  console.log(productList);
+  const productList = [...products].sort((prod1, prod2) => {
+    if (sort === 0) {
+      return prod1.arrivalDate.seconds - prod2.arrivalDate.seconds;
+    } else if (sort === 1) {
+      return prod1.price - prod2.price;
+    } else {
+      return prod2.price - prod1.price;
+    }
+  });
+
+  const handleSort = (e) => setSort(+e.target.value);
+
   return (
     <PageLayout title="Shop">
-      <div className={classes["top-bar"]}>
-        <h2>Ecommerce Accessories & Fashion Items</h2>
-        <label>
-          Per Page:
-          <input className={classes.perpage} type="text" />
-        </label>
-        <label>
-          Sort by:
-          <select>
-            <option>-</option>
-            <option>Price: Lowest first</option>
-            <option>Price: Highest first</option>
-            <option>Arrival Date</option>
-          </select>
-        </label>
-        <label>
-          View:
-          <button
-            onClick={() => {
-              setGridView(true);
-            }}
-          >
-            <img src={icons.gridView} />
-          </button>
-          <button
-            onClick={(e) => {
-              setGridView(false);
-            }}
-          >
-            <img src={icons.listView} />
-          </button>
-        </label>
-        <input type="text" />
-        {gridView && (
+      <section className={classes["temporary-container"]}>
+        <div className={classes["top-bar"]}>
+          <h2>Ecommerce Accessories & Fashion Items</h2>
+          <label>
+            Per Page:
+            <input className={classes.perpage} type="text" />
+          </label>
+          <label>
+            Sort by:
+            <select onChange={handleSort}>
+              <option value="0">Arrival Date</option>
+              <option value="1">Price: Lowest first</option>
+              <option value="2">Price: Highest first</option>
+            </select>
+          </label>
+          <label>
+            View:
+            <button
+              onClick={() => {
+                setGridView(true);
+              }}
+            >
+              <img src={icons.gridView} />
+            </button>
+            <button
+              onClick={(e) => {
+                setGridView(false);
+              }}
+            >
+              <img src={icons.listView} />
+            </button>
+          </label>
+          <input type="text" />
           <button>
             <img src={icons.filter} />
           </button>
+        </div>
+        {gridView ? (
+          <GridView productList={productList} />
+        ) : (
+          <ListView productList={productList} />
         )}
-      </div>
-      {gridView ? (
-        <GridView productList={productList} />
-      ) : (
-        <ListView productList={productList} />
-      )}
+      </section>
     </PageLayout>
   );
 };
