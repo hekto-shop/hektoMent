f69319dd8f2246e426ccfea4ead5f91552e10ec7
Giorgi Beriashvili <giorgi_beriashvili@epam.com>
2022-02-02 22:42:04 +0400

    feat: add feature products slider

--- src/components/FeaturedSlider/FeaturedSlider.js
+++ src/components/FeaturedSlider/FeaturedSlider.js
@@ -1,13 +1,99 @@
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Carousel from "react-elastic-carousel";
+import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
 
 import styles from "./FeaturedSlider.module.scss";
+import "./slider.css";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1, pagination: false },
+  { width: 400, itemsToShow: 2 },
+  { width: 750, itemsToShow: 3 },
+  { width: 900, itemsToShow: 4 },
+];
 
 const FeaturedSlider = () => {
+  const [hovering, setHovering] = useState(false);
+  const [current, setCurrent] = useState(0);
+  const history = useHistory();
   const products = useSelector((state) => state.productsReducer.products);
+  const currency = useSelector((state) => state.productsReducer.currency);
 
-  console.log(products);
+  const featuredProducts = [...products]
+    .sort((a, b) => {
+      if (a.rating > b.rating) return -1;
+      if (a.rating < b.rating) return 1;
+      return 0;
+    })
+    .slice(0, 12);
 
-  return <section className={styles.test}></section>;
+  const hoverHandler = (ind) => {
+    setHovering(true);
+    setCurrent(ind);
+  };
+
+  const hoverCancelHandler = () => {
+    setHovering(false);
+  };
+
+  const featPd = featuredProducts.map((item, ind, arr) => {
+    return (
+      <Card
+        key={ind}
+        className={styles.card}
+        sx={{ width: 600 }}
+        onMouseEnter={() => hoverHandler(ind)}
+        onMouseLeave={hoverCancelHandler}
+      >
+        <CardActionArea>
+          <div className={styles["card-media"]}>
+            <CardMedia
+              component="img"
+              alt={item.productImage}
+              height="200"
+              image={item.productImage}
+            />
+            {ind === current && hovering ? (
+              <button
+                className={styles["card-media__btn"]}
+                onClick={() => {
+                  history.push(`/product/${item.productCode}`);
+                }}
+              >
+                View Details
+              </button>
+            ) : null}
+          </div>
+          <CardContent sx={{ p: 0 }}>
+            <div className={styles["card-description"]}>
+              <h4 className={styles["card-description__title"]}>{item.name}</h4>
+              <div className={styles["card-description__colors"]}>
+                {item.color.map((col, ind) => {
+                  return (
+                    <div
+                      style={{ backgroundColor: col }}
+                      className={styles["card-description__col"]}
+                      key={ind}
+                    ></div>
+                  );
+                })}
+              </div>
+              <p className={styles["card-description__code"]}>Code - Y523201</p>
+              <p className={styles["card-description__price"]}>
+                {currency}: {item.price}
+              </p>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  });
+
+  console.log(featuredProducts);
+
+  return <Carousel breakPoints={breakPoints}>{featPd}</Carousel>;
 };
 
 export default FeaturedSlider;
