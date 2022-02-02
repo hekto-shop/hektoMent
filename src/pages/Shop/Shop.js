8bc1f559ae02c0a150e694d129377b3f299e11f9
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-02-02 20:42:19 +0400

    feat: set up pagination

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -12,8 +12,14 @@ const Shop = () => {
   const [gridView, setGridView] = useState(true);
   const [sort, setSort] = useState(0); // 0-Arrival date; 1-Price:Lowest->Highest; 2-Price:Highest->Lowest;
   const [searchValue, setSearchValue] = useState("");
-
+  const [perPage, setPerPage] = useState(16);
+  const [currentPage, setCurrentPage] = useState(1);
   const products = useSelector((state) => state.productsReducer.products);
+  const totalPages = Math.trunc(products.length / perPage) + 1;
+
+  console.log(currentPage);
+  console.log(totalPages);
+  console.log(perPage);
 
   let productList = [...products].sort((prod1, prod2) => {
     if (sort === 0) {
@@ -33,9 +39,18 @@ const Shop = () => {
     );
   });
 
+  productList = productList.slice(
+    (currentPage - 1) * perPage,
+    perPage * currentPage
+  );
+
   const handleSort = (e) => setSort(+e.target.value);
   const handleSearch = (e) => setSearchValue(e.target.value);
-
+  const handlePerPage = (e) => setPerPage(+e.target.value);
+  const handlePagination = (e) => {
+    window.scrollTo({ top: 400, behavior: "smooth" });
+    setCurrentPage((prevState) => prevState + +e.target.value);
+  };
   return (
     <PageLayout title="Shop">
       <section className={classes["temporary-container"]}>
@@ -43,7 +58,12 @@ const Shop = () => {
           <h2>Ecommerce Accessories & Fashion Items</h2>
           <label>
             Per Page:
-            <input className={classes.perpage} type="text" />
+            <input
+              className={classes.perpage}
+              type="number"
+              placeholder="16"
+              onChange={handlePerPage}
+            />
           </label>
           <label>
             Sort by:
@@ -80,6 +100,23 @@ const Shop = () => {
         ) : (
           <ListView productList={productList} />
         )}
+        <div className={classes.pagination}>
+          <button
+            disabled={currentPage === 1}
+            onClick={handlePagination}
+            value="-1"
+          >
+            Previous Page
+          </button>
+          <span>{`${currentPage} / ${totalPages}`}</span>
+          <button
+            disabled={currentPage === totalPages}
+            onClick={handlePagination}
+            value="1"
+          >
+            Next Page
+          </button>
+        </div>
       </section>
     </PageLayout>
   );
