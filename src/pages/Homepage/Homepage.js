0ba2b05e4d45b3c9545d46fa397512c8e6624ec6
Giorgi Beriashvili <giorgi_beriashvili@epam.com>
2022-02-03 22:21:39 +0400

    feat: add shopex section

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from "react";
 import PageLayout from "../../containers/PageLayout";
 import MainSlider from "../../components/MainSlider";
 import FeaturedSlider from "../../components/FeaturedSlider";
+import Shopex from "../../components/Shopex";
 
 import PageContainer from "../../containers/PageContainer";
 import styles from "./Homepage.module.scss";
@@ -10,11 +11,19 @@ const Homepage = () => {
   return (
     <PageLayout title="Home" isHomepage={true}>
       <MainSlider />
-      <PageContainer className={styles["featured-section"]}>
-        <div className={styles["featured-title"]}>
-          <h2>Featured Products</h2>
-        </div>
-        <FeaturedSlider />
+      <PageContainer>
+        <section className={styles["featured-section"]}>
+          <div className={styles["featured-section__title"]}>
+            <h2>Featured Products</h2>
+          </div>
+          <FeaturedSlider />
+        </section>
+        <section className={styles["shopex-section"]}>
+          <div className={styles["shopex-section__title"]}>
+            <h2>What Shopex Offer!</h2>
+          </div>
+          <Shopex />
+        </section>
       </PageContainer>
       <h1>Homepage</h1>
     </PageLayout>
