7fc783e428ad6ebb417a8d84df8d20ea092d3325
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-02-06 04:41:19 +0400

    feat: update page URL based on search values on shop page. update shop view based on url search params

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 import {
   makeSlice,
   productListReducer,
@@ -15,30 +16,57 @@ import Pagination from "../../components/Pagination/Pagination";
 import PageContainer from "../../containers/PageContainer";
 
 const Shop = () => {
-  const [gridView, setGridView] = useState(true);
+  const [view, setView] = useState("grid");
   const [sortType, setSortType] = useState(0); // 0-Arrival date; 1-Price:Lowest->Highest; 2-Price:Highest->Lowest;
   const [searchValue, setSearchValue] = useState("");
   const [perPage, setPerPage] = useState(16);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
+      if (params.get("view")) setView(params.get("view"));
+      if (params.get("sort")) setSortType(+params.get("sort"));
+      if (params.get("keyword")) setSearchValue(params.get("keyword"));
+      if (params.get("perpage") && Number.isInteger(+params.get("perpage")))
+        setPerPage(+params.get("perpage"));
+      if (params.get("page") && Number.isInteger(+params.get("page")))
+        setCurrentPage(+params.get("page"));
+    } else {
+      setView("grid");
+      setSortType(0);
+      setSearchValue("");
+      setPerPage(16);
+      setCurrentPage(1);
+    }
+  }, [location]);
+
   const products = useSelector((state) => state.productsReducer.products); // Can't be modified.
   const filteredList = productListReducer([...products], sortType, searchValue);
-
   const totalPages = Math.trunc(filteredList.length / perPage) + 1;
-
   const productList = makeSlice(filteredList, currentPage, perPage);
 
+  const updateURL = (name, value) => {
+    const params = new URLSearchParams(location.search);
+    params.set(name, value);
+    const newURL = `?${params.toString()}`;
+    history.push({ pathname: location.pathname, search: newURL });
+  };
+
   // Event Handlers for <ShopSettings/>
-  const handleSort = (e) => setSortType(+e.target.value);
-  const handleSearch = (e) => setSearchValue(e.target.value);
-  const handlePerPage = (e) => setPerPage(+e.target.value);
-  const handleGridView = () => setGridView(true);
-  const handleListView = () => setGridView(false);
+  const handleSort = (e) => updateURL("sort", e.target.value);
+  const handleSearch = (e) => updateURL("keyword", e.target.value);
+  const handlePerPage = (e) => updateURL("perpage", e.target.value);
+  const handleGridView = () => updateURL("view", "grid");
+  const handleListView = () => updateURL("view", "list");
 
   // Event Handlers for <Pagination/>
   const handlePagination = (e) => {
     window.scrollTo({ top: 400, behavior: "smooth" });
-    setCurrentPage((prevState) => prevState + +e.target.value);
+    updateURL("page", currentPage + +e.target.value);
   };
 
   return (
@@ -52,7 +80,7 @@ const Shop = () => {
           handleListView={handleListView}
           numberOfProducts={products.length}
         />
-        {gridView ? (
+        {view === "grid" ? (
           <GridView productList={productList} />
         ) : (
           <ListView productList={productList} />
