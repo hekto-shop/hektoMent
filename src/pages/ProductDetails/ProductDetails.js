b4d0051b80bdd95fa2c3336dc38523f917ad9d49
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-03-02 14:57:46 +0400

    feat: add Loader component in UI folder

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -6,6 +6,7 @@ import PageLayout from "../../containers/PageLayout";
 import ProductCard from "./ProductCard";
 import MoreInfo from "./MoreInfo";
 import RelatedProducts from "./RelatedProducts";
+import Loader from "../../components/UI/Loader";
 
 import { scrollTo } from "../../helpers/smooth-scroll";
 
@@ -14,12 +15,25 @@ const ProductDetails = () => {
   const id = params.id;
   const productList = useSelector((store) => store.productsReducer.products);
   const currency = useSelector((store) => store.productsReducer.currency);
-  const product = [...productList].find((prod) => prod.productCode === id);
+  // const product = [...productList].find((prod) => prod.productCode === id);
+  const [product, setProduct] = useState(null);
 
-  useEffect(() => scrollTo(), []);
+  useEffect(() => {
+    scrollTo();
+    setProduct([...productList].find((prod) => prod.productCode === id));
+  }, [productList, id]);
+
+  if (!product) {
+    return (
+      <PageLayout title="Product Details">
+        <Loader />
+      </PageLayout>
+    );
+  }
+
   return (
     <PageLayout title="Product Details">
-      <ProductCard product={product} currency={currency} />
+      <ProductCard product={product} currency={currency} />}
       <MoreInfo product={product} />
       <RelatedProducts product={product} />
     </PageLayout>
