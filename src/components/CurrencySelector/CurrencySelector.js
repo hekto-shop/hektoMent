6c3bf184339463c8783c5ec873e34fbbce8f6883
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-03-21 15:50:49 +0400

    fix: Currency selector UI

---
+++ src/components/CurrencySelector/CurrencySelector.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changeCurrency } from "../../store/thunk";
+import classes from "./CurrencySelector.module.scss";
+import * as icons from "../../assets/icons";
+
+const CurrencySelector = () => {
+  const currency = useSelector((store) => store.productsReducer.currency);
+  const [showOptions, setShowOptions] = useState(false);
+  const allCurrencies = ["USD", "EUR", "GEL"];
+  const notSelected = allCurrencies.filter((cur) => cur !== currency);
+  const dispatch = useDispatch();
+  const handleCurrency = (e) => {
+    dispatch(changeCurrency(e.target.dataset.value));
+    toggleOptions();
+  };
+
+  const toggleOptions = () => {
+    setShowOptions((prevState) => !prevState);
+  };
+
+  const arrowClass = showOptions ? classes.rotated : "";
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.label} onClick={toggleOptions}>
+        <span>{currency}</span>
+        <img className={arrowClass} src={icons.arrow} alt="toggle" />
+      </div>
+      {showOptions && (
+        <div onClick={handleCurrency} className={classes.options}>
+          {notSelected.map((el) => (
+            <span data-value={el}>{el}</span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CurrencySelector;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./Header.module.scss";
 import { useSession } from "../../contexts/auth-context";
-import { useSelector, useDispatch } from "react-redux";
-import { changeCurrency } from "../../store/thunk";
 
 import * as icons from "../../assets/icons";
 import PageContainer from "../../containers/PageContainer";
 import Navigation from "./Navigation";
 import Searchbar from "./Searchbar";
+import CurrencySelector from "../CurrencySelector";
 
-import { useTheme } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { ColorModeContext } from "../../theme/colorModeContext";
 
 const Header = () => {
   const { user } = useSession();
-  const { currency } = useSelector((store) => store.productsReducer);
-  const dispatch = useDispatch();
-  const handleCurrency = (e) => {
-    dispatch(changeCurrency(e.target.value));
-  };
-  
+
   const theme = useTheme();
-  const titleColor = {"color": theme.palette.text.hektoTitle};
-  
+  const titleColor = { color: theme.palette.text.hektoTitle };
+
   const colorMode = React.useContext(ColorModeContext);
   return (
     <header className={classes.header}>
@@ -43,17 +37,7 @@ const Header = () => {
             </address>
           </div>
           <div className={classes.controls}>
-            <div>
-              <select
-                className={classes.select}
-                onChange={handleCurrency}
-                value={currency}
-              >
-                <option value="USD">USD</option>
-                <option value="GEL">GEL</option>
-                <option value="EUR">EUR</option>
-              </select>
-            </div>
+            <CurrencySelector />
             <Link to={user ? "/profile" : "/login"}>
               <h4>{user ? user.displayName : "Login"}</h4>
               <img src={icons.user} alt="User" />
@@ -65,8 +49,16 @@ const Header = () => {
             <Link to="/cart">
               <img src={icons.cart} alt="cart" />
             </Link>
-            <IconButton onClick={() => {colorMode.toggleColorMode()}}>
-              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            <IconButton
+              onClick={() => {
+                colorMode.toggleColorMode();
+              }}
+            >
+              {theme.palette.mode === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <Brightness4Icon />
+              )}
             </IconButton>
           </div>
         </PageContainer>
