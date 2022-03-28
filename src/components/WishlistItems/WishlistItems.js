0fa34e1f3dd08ee0efdab16417172c816692023a
Besarion Gugushvili <besarion_gugushvili@epam.com>
2022-03-28 13:18:12 +0400

    feat: add wishlist items on wishlist page

--- src/components/WishlistItems/WishlistItems.js
+++ src/components/WishlistItems/WishlistItems.js
@@ -1,20 +1,72 @@
 import React from "react";
 import styles from "./WishlistItems.module.scss";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Button, Card, CardActionArea, CardContent, CardMedia, Grid, Item, } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { formatCurrency } from "../../helpers/format-number";
+import { makeStyles } from "@mui/styles";
+import { removeFromFavorites } from "../../store/thunk";
 
-import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
-const WishlistItems = (props) => {
-  let wishlistArray = props.wishlistArray;
-  console.log(wishlistArray);
-  return (
-    <Card>
-        <CardActionArea>
-            <CardMedia
+import * as icons from "../../assets/icons";
+
+const useStyles = makeStyles({
+  image: {
+    width: 'auto',
+    maxHeight: '150px',
+    gridColumn: '1/2',
+    margin:'auto'
+  },
+  cardAction: {
+    display: 'grid',
+    gridTemplateRows: '200px',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'
+  }
+});
+
+const WishlistItems = () => {
+  
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  let wishlistItems = useSelector((state)=> state.cartReducer.favorites);
+  const currency = useSelector((state) => state.productsReducer.currency);
+  console.log(wishlistItems);
+
+  
+  const handleRemoveFavorite = (product) => {
+    dispatch(removeFromFavorites(product));
+  };
+
+  const items = wishlistItems.map((item) => 
+    <Card key={item.productCode} className={styles["card"]} >
+      <CardActionArea className={classes.cardAction}>
+        <div className={styles["image-remove"]}>
+          <CardMedia
             component="img"
-            height="200"/>
-            Wishlist
-        </CardActionArea>
+            image={item.productImage}
+            className={classes.image}
+          />
+          <span onClick={() => handleRemoveFavorite(item)}>
+            <img src={icons.deleteIcon} alt="remove" />
+          </span>
+        </div>
+        <CardContent className={styles["card-description"]}>
+          <h3 className={styles["card-description-title"]}>{item.name}</h3>
+          <h3 className={styles["card-description-code"]} >Code - Y523201</h3>
+          <h3 className={styles["card-description-price"]} >
+              {formatCurrency(item.price, currency)}
+          </h3>
+        </CardContent>
+        <Button onClick={() => {history.push(`/product/${item.productCode}`)}} variant="contained">View Details</Button>
+      </CardActionArea> 
     </Card>
+  )
+  return (
+    <div className={styles["container"]}>
+      {items}
+    </div>
   );
 };
 
 export default WishlistItems;
+
--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -2,8 +2,8 @@ import React from "react";
 import PageContainer from "../../containers/PageContainer";
 import PageLayout from "../../containers/PageLayout";
 import WishlistItems from "../../components/WishlistItems";
-
 const wishlistArray = [{name:'first', price:'10', code:'1'}, {name:'first', price:'10', code:'1'}];
+
 
 const Wishlist = () => {
   return (
