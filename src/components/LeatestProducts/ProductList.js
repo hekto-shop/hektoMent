3fbd32674f5998c9ae2c1829b5d74cb3ff8d2aa5
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-03-29 14:39:45 +0400

    feat: Add idle timer, logout user after X seconds

--- src/components/LeatestProducts/ProductList.js
+++ src/components/LeatestProducts/ProductList.js
@@ -6,11 +6,11 @@ import Controls from "../Controls";
 import * as images from "../../assets/images";
 import { formatCurrency } from "../../helpers/format-number";
 import classes from "./ProductList.module.scss";
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 
 const ProductList = (props) => {
   const theme = useTheme();
-  const textColor = {"color": theme.palette.text.primary};
+  const textColor = { color: theme.palette.text.primary };
   const productList = props.productList;
   const currency = useSelector((store) => store.productsReducer.currency);
   const sales = useSelector((store) => store.salesReducer.sales);
@@ -22,7 +22,7 @@ const ProductList = (props) => {
     </span>
   );
 
-  const items = productList.map((item) => {
+  const items = productList.map((item, idx) => {
     const isOnSale = sales.some(
       (elem) => elem.product.productCode === item.productCode
     );
@@ -34,7 +34,7 @@ const ProductList = (props) => {
     const oldPrice = (item.price / (100 - discount)) * 100;
 
     return (
-      <div className={classes.card}>
+      <div key={idx} className={classes.card}>
         <div className={classes["image-container"]}>
           {isOnSale && saleBadge}
           <Controls layout="vertical" product={item} />
@@ -42,8 +42,10 @@ const ProductList = (props) => {
         </div>
         <Link to={`/product/${item.productCode}`}>
           <div className={classes.description}>
-            <span className={classes["item-name"]}  style={textColor}>{item.name}</span>
-            <span className={classes["item-price"]}  style={textColor}>
+            <span className={classes["item-name"]} style={textColor}>
+              {item.name}
+            </span>
+            <span className={classes["item-price"]} style={textColor}>
               <span> {formatCurrency(item.price, currency)} </span>
               {isOnSale && (
                 <span className={classes["old-price"]}>
